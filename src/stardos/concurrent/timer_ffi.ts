8d61d4c7192ba6c6bf2d52340184c9aca5de1510
acandoo <117209328+acandoo@users.noreply.github.com>
2026-01-31 22:33:44 -0500

    fix(timer): move intervalTimer scope up

--- src/stardos/concurrent/timer_ffi.ts
+++ src/stardos/concurrent/timer_ffi.ts
@@ -21,11 +21,11 @@ export function sleep(duration): Future<undefined> {
 // todo figure out how to solve timing issue
 export function interval(duration): FutureStream {
   const durationMs = to_milliseconds(duration)
+  let intervalTimer: NodeJS.Timeout | number = 0
   return FutureStream$FutureStream(() => {
     // The input callback to the FutureStream constructor executes
     // immediately and is not deferred until the FutureStream is awaited,
     // so the timer has to be created inside the execute function.
-    let intervalTimer: NodeJS.Timeout | number = 0
     let timerCb: () => void
     return {
       execute: () =>
